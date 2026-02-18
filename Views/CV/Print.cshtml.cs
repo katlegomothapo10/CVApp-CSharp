bc723886db7cb113d46dd23d8af1b961a247fc83
katlegomothapo10 <katlegomothapo10@gmail.com>
2026-02-18 09:58:36 +0200

    Views

---
+++ Views/CV/Print.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CVApp.Views.CV
+{
+    public class PrintModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
